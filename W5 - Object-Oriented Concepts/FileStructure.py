2097822be1d6c2520c76be781f715aa7f250bf2f
aaronizzl3 <58277262+aaronizzl3@users.noreply.github.com>
2021-10-21 09:42:21 +0100

    Create FileStructure.py

---
+++ W5 - Object-Oriented Concepts/FileStructure.py
@@ -0,0 +1,28 @@
+'''
+This file is responsible for... XYZ
+'''
+
+# Imports - Standard Library, Third Party, Local
+
+
+# Global Variables/Structures
+
+
+# Class Definitions
+# Class Example
+class MyClass:
+    # Class Attributes
+    myAttribute = None
+    x = 1
+
+    # Class Constructor
+    def __init__(self):
+        pass
+
+    # Class Methods
+    def myFunction(self):
+        print(self.x)
+
+
+# Main
+
