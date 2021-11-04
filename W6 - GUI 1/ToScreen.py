96bd7550c405acc55670a0e2b4e588b96b5f8fc2
aaronizzl3 <58277262+aaronizzl3@users.noreply.github.com>
2021-11-04 11:28:13 +0000

    We do it live
    N/A

---
+++ W6 - GUI 1/ToScreen.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def addition():
+    x = num_one.get()
+    y = num_two.get()
+    ans = x + y
+    lbl_answer.config(text=str(ans))
+
+
+def reset():
+    ent_num_one.delete(0, 'end')
+    ent_num_two.delete(0, 'end')
+    lbl_answer.config(text="0")
+
+
+root = tk.Tk()
+
+num_one = tk.IntVar(value="")
+num_two = tk.IntVar(value="")
+
+ent_num_one = ttk.Entry(root, textvariable=num_one)
+ent_num_one.pack()
+ent_num_two = ttk.Entry(root, textvariable=num_two)
+ent_num_two.pack()
+
+lbl_answer = ttk.Label(root, text="0", padding=(10,10))
+lbl_answer.pack()
+
+btn_get_answer = ttk.Button(root, text="Add", command=addition).pack()
+btn_reset = ttk.Button(root, text="Reset", command=reset).pack()
+
+root.mainloop()
