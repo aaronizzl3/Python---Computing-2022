c27b522f52549f9bb2dd3b569e5a59a648e13e3d
aaronizzl3 <58277262+aaronizzl3@users.noreply.github.com>
2021-11-18 11:17:26 +0000

    The Kyle G Wizard-G

---
+++ W8 - SQLite3/Kyle G Database/createTables.py
@@ -0,0 +1,26 @@
+# Import Library
+import sqlite3
+
+# Key Steps - Connection, Cursor, Execute, Commit, Close
+
+# 1 - Connect
+connection = sqlite3.connect("myDatabase.db")
+
+# 2 - Cursor (this is used to point queries at the database)
+cursor = connection.cursor()
+
+# 3 - Execute our Query
+try:
+    query = '''
+    CREATE TABLE tblUsers(
+    id INTEGER PRIMARY KEY,
+    username TEXT);'''
+    cursor.execute(query)
+except sqlite3.Error as error:
+    print(f"Error: {error}")
+
+# 4 - Commit (makes our data permanent in the database)
+connection.commit()
+
+# 5 - Close connection
+connection.close()
